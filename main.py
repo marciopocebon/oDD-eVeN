1bf6130a6251d034279bc17a916f71b31774c976
hhhrrrttt222111 <43471295+hhhrrrttt222111@users.noreply.github.com>
2020-02-13 16:16:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import random
-print("\nIn this game you get to play batting first. The rest is just normal odd or even game.\n")
-sum1=0
-for i in range(100):
-    human=int(input("Enter a number between 1 and 6\n"))
-    comp=random.randint(1,6)
-    print(comp)
-    if human==comp:
-        print("You are out")
-        print("You have scored "+str(sum1))
-        break
-    else:
-        sum1+=human
 
-sum2=0
-for i in range(100):
-    human=int(input("Enter a number between 1 and 6\n"))
-    comp=random.randint(1,6)
+runs = 0
+def batting():
+
+    for i in range(120):
+        global runs
+        user = int(input("Enter a number between 1 and 6 \n"))
+        comp = random.randint(1, 6)
+        print(comp)
+        if user == comp:
+            print("You are out")
+            print("You have scored " + str(runs))
+            break
+        else:
+            runs += user
+
+
+
+
+print(" \n In this game you get to play batting first. The rest is just normal odd or even game. \n")
+
+
+batting();
+target = runs + 1
+
+sum2 = 0
+for i in range(120):
+    user = int(input("Enter a number between 1 and 6 \n"))
+    comp = random.randint(1,6)
     print(comp)
-    if human==comp:
+    if user == comp:
         print("Computer is out")
-        print("Computer has scored "+str(sum2))
+        print("Computer has scored " + str(sum2))
         break
     else:
-        sum2+=comp
-    if sum2>sum1:
+        sum2 += comp
+
+    if sum2 > target:
         print("\nComputer wins")
         break
 
-if sum1>sum2:
-    print("\nYou win")
+if target > sum2:
+    print("\n You win")
+
+
+
